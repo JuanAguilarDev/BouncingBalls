77d1060ba46eb8e809f6f5c4f996a97ae4044837
JuanAguilarDev <80842563+JuanAguilarDev@users.noreply.github.com>
2022-03-01 20:06:26 -0600

    Add files via upload

--- juego.js
+++ juego.js
@@ -12,114 +12,61 @@
 
     var ctx = null;
 
-    var Generador = {
-        pelotas: [],
-        llenar: function(){
-            while(this.pelotas.length < 3){
-                const pelota = new Pelota( aleatorio(120, 400),  aleatorio(120, 400)); 
-                this.pelotas.push(pelota);
-            }
-        }
-    }
-    
     var Juego = {
-        // Configuración inicial
+
         canvas: document.getElementById('canvas'),
 
         configurar: function() {
-            if(this.canvas.getContext) { 
-                // Varariables de configuración 
-                ctx = this.canvas.getContext('2d'); 
-                // Cache width y height del canvas para ahorrar poder de procesamiento 
-                this.width = this.canvas.width; 
-                this.height = this.canvas.height; 
-                // Ejecutar el juego 
-                //Pantalla.bienvenida(); 
-                //this.canvas.addEventListener('click', this.ejecutaJuego, false); 
-                this.init(); 
-                Ctrl.init(); 
-                
-                
-            } 
+            if(this.canvas.getContext) {
+                ctx = this.canvas.getContext('2d');
+                // Cache width y height del canvas para ahorrar poder de procesamiento
+                this.width = this.canvas.width;
+                this.height = this.canvas.height;
+                // Ejecutar el juego
+                Pantalla.bienvenida();
+                this.canvas.addEventListener('click', this.ejecutaJuego,false);
+                //this.init();
+                Ctrl.init();
+            }
         },
 
-        ejecutaJuego: function() { 
-            Juego.canvas.removeEventListener('click', Juego.ejecutaJuego, false); 
+        ejecutaJuego: function() {
+            Juego.canvas.removeEventListener('click', Juego.ejecutaJuego, false);
             Juego.init();
-            Juego.animar(); 
+            Juego.animar();
         },
 
-        reiniciarJuego: function() { 
-            Juego.canvas.removeEventListener('click', Juego.reiniciarJuego, false); 
-            Juego.init(); 
+        reiniciarJuego: function() {
+            Juego.canvas.removeEventListener('click', Juego.reiniciarJuego,false);
+            Juego.init();
         },
 
         animar: function() {
-            Juego.play = requestAnimFrame(Juego.animar);
+                Juego.play = requestAnimFrame(Juego.animar);
             Juego.dibujar();
         },
-
-
-        // Configurar objetos iniciales
         init: function() {
             Fondo.init();
-            Tablero.init();  
-            Pala.init();          
+            Pelota.init();
+            P2.init();
+            P3.init();
+            Tablero.init();
+            Pala.init();
             Ladrillos.init();
-            Generador.llenar();
-            for(var i=0; i<Generador.pelotas.length; i++){
-                Generador.pelotas[i].init(); 
-            }
-            this.animar(); 
+            this.animar();
         },
-
-        subirNivel: function() { 
-            Tablero.nivel += 1; 
-            Ladrillos.init(); 
-            for(var i=0; i<Generador.pelotas.length; i++){
-                Generador.pelotas[i].init(); 
-            }
-            Pala.init(); 
-        }, 
-        
-        limiteNivel: function(niv){ 
-            return niv > 5 ? 5: niv; 
-        }
-        ,
-
-        // dibujar maneja toda la logica para actualizar y dibujar los objetos
         dibujar: function() {
             ctx.clearRect(0, 0, this.width, this.height);
-
             Fondo.dibujar();
             Ladrillos.dibujar();
             Pala.dibujar();
+            Pelota.dibujar();
+            P2.dibujar();
+            P3.dibujar();
             Tablero.dibujar();
-            for(var i=0; i<Generador.pelotas.length; i++){
-                Generador.pelotas[i].dibujar(); 
-            }
-            
         }
     };
-
-
-    var Tablero = {
-        init: function() { 
-            this.nivel = 1; 
-            this.marcador = 0; 
-        }, 
-        
-        dibujar: function() { 
-            ctx.font = '12px helvetica, arial';
-            ctx.fillStyle = 'white'; 
-            ctx.textAlign = 'left'; 
-            ctx.fillText('Marcador: ' + this.marcador, 5, Juego.height - 5); 
-            ctx.textAlign = 'right'; 
-            ctx.fillText('Niv:' + this.nivel, Juego.width - 5, Juego.height - 5); 
-        }
-    };
-
-
+    
     var Fondo = {
         init: function() {
             this.ready = false;
@@ -137,46 +84,17 @@
         }
     };
 
-    var Pantalla = {
-        bienvenida: function() { 
-            this.text = 'REBOTE RICOCHET'; 
-            this.textSub = 'Clic para Iniciar'; 
-            this.textColor = 'white'; 
-            this.create(); 
-        }, 
-        
-        crear: function() { 
-            ctx.fillStyle = 'black'; 
-            ctx.fillRect(0, 0, Juego.width, Juego.height); 
-            ctx.fillStyle = this.textColor;
-            ctx.textAlign = 'center'; 
-            ctx.font = '40px helvetica, arial'; 
-            ctx.fillText(this.text, Juego.width / 2, Juego.height / 2); 
-            ctx.fillStyle = '#999999'; 
-            ctx.font = '20px helvetica, arial'; 
-            ctx.fillText(this.textSub, Juego.width / 2, Juego.height / 2 + 30); 
-        },
-
-        juegoterminado: function() { 
-            this.text = 'Juego Terminado'; 
-            this.textSub = 'Clic para Reintentar'; 
-            this.textColor = 'red'; 
-            this.crear(); 
-        }
-    };
-
     var Ladrillos = {
         gap: 2,
         col: 5,
         w: 80,
         h: 15,
-
         init: function() {
-            this.row = 2 + Juego.limiteNivel(Tablero.nivel); 
-            this.total = 0; 
-            this.count = [this.row]; 
-            for (var i = this.row;i--;){ 
-                this.count[i] = [this.col]; 
+            this.row = 3;
+            this.total = 0;
+            this.count = [this.row];
+            for(var i = this.row; i--;){
+                this.count[i] = [this.col];
             }
         },
 
@@ -185,17 +103,15 @@
             for ( i= this.row; i--;){
                 for(j = this.col; j--;){
                     if(this.count[i][j] !== false){
-                        for(var i=0; i<Generador.pelotas.length; i++){
-                            if (Generador.pelotas[i].x >= this.x(j)  &&
-                            Generador.pelotas[i].x <= (this.x(j) + this.w) &&
-                            Generador.pelotas[i].y >= this.y(i) &&
-                            Generador.pelotas[i].y <= (this.y(i) + this.h)) {
+                        if ((Pelota.x || P2.x  || P3.x)>= this.x(j)  &&
+                            (Pelota.x || P2.x  || P3.x) <= (this.x(j) + this.w) &&
+                            (Pelota.y || P2.y  || P3.y)>= this.y(i) &&
+                            (Pelota.y || P2.y  || P3.y)<= (this.y(i) + this.h)) {
                                 this.collide(i, j);
                                 continue
                             }
                             ctx.fillStyle = this.gradient(i);
                             ctx.fillRect(this.x(j), this.y(i), this.w, this.h);
-                        }
                     }
                 }
             } 
@@ -206,11 +122,11 @@
 
         collide: function(i, j) {
             Tablero.marcador +=1;
-            this.total += 1; 
-            this.count[i][j] = false; 
-            for(var i =0; i<Generador.pelotas.length; i++){
-                Generador.pelotas[i].sy = -Generador.pelotas[i].sy;
-            }
+            this.total += 1;
+            this.count[i][j] = false;
+            Pelota.sy = -Pelota.sy;
+            P2.sy = -P2.sy; 
+            P3.sy = -P3.sy;
         },
 
         x: function(row){
@@ -223,17 +139,13 @@
 
         gradient: function(row) {
             switch(row) {
-                case 0:
-                    return this.gradientPurple ? this.gradientPurple :
+                case 0: return this.gradientPurple ? this.gradientPurple :
                         this.gradientPurple = this.makeGradient(row, '#bd06f9','#9604c7');
-                case 1:
-                    return this.gradientRed ? this.gradientRed :
+                case 1: return this.gradientRed ? this.gradientRed :
                         this.gradientRed = this.makeGradient(row, '#f9064a','#c7043b');
-                case 2:
-                    return this.gradientGreen ? this.gradientGreen :
+                case 2: return this.gradientGreen ? this.gradientGreen :
                         this.gradientGreen = this.makeGradient(row, '#05fa15','#04c711');
-                default:
-                    return this.gradientOrange ? this.gradientOrange :
+                default: return this.gradientOrange ? this.gradientOrange :
                         this.gradientOrange = this.makeGradient(row, '#faa105','#c77f04');
             }
         },
@@ -244,33 +156,19 @@
             grad.addColorStop(0, color1);
             grad.addColorStop(1, color2);
             return grad;
-        }
-
-        
+        }        
     };
 
-    function aleatorio(inferior, superior) {
-        var numPosibilidades = superior - inferior;
-        var aleatorio = Math.random() * (numPosibilidades + 1);
-        aleatorio = Math.floor(aleatorio);
-        return inferior + aleatorio;
-    }
-
     var Pelota = {
-        
-
-        init: function(x, y){
-            this.x = x; 
-            this.y = y; 
-            this.sx = 1 + (0.4 * Tablero.nivel); 
-            this.sy = -1.5 - (0.4 * Tablero.nivel);
-            this.r = 10;
+        r: 10,
+        init: function() {
+            this.x = 100;
+            this.y = 100;
+            this.sx = 2;
+            this.sy = -2;
         },
 
-        
-
         dibujar: function() {
-            
             this.edges();
             this.collide();
             this.move();
@@ -278,18 +176,17 @@
             ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
             ctx.closePath();
             ctx.fillStyle = "#eee";
-            ctx.fill(); 
-                   
+            ctx.fill();
         },
 
         edges: function(){
             if(this.y < 1){
                 this.y = 1;
                 this.sy = -this.sy;
-            } else if (this.y > Juego.height) { // Borde inferior
+            } else if (this.y > Juego.height) {
                 this.sy = this.sx = 0;
                 this.y = this.x = 1000;
-                Pantalla.gameover();
+                Pantalla.juegoterminado();
                 canvas.addEventListener('click', Juego.reiniciarJuego, false);
                 return;
             }
@@ -301,7 +198,6 @@
                 this.sx = -this.sx;
             }
         },
-
         collide: function(){
             if(this.x >= Pala.x 
                 && this.x <= (Pala.x + Pala.w) 
@@ -318,6 +214,142 @@
         }
     };
 
+    var P2 = {
+        r: 10,
+        init: function() {
+            this.x = 120;
+            this.y = 120;
+            this.sx = 2;
+            this.sy = -2;
+        },
+
+        dibujar: function() {
+            this.edges();
+            this.collide();
+            this.move();
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+            ctx.closePath();
+            ctx.fillStyle = "#eee";
+            ctx.fill();
+        },
+
+        edges: function(){
+            if(this.y < 1){
+                this.y = 1;
+                this.sy = -this.sy;
+            } else if (this.y > Juego.height) {
+                this.sy = this.sx = 0;
+                this.y = this.x = 1;
+                Pantalla.juegoterminado();
+                canvas.addEventListener('click', Juego.reiniciarJuego, false);
+                return;
+            }
+            if(this.x < 1){
+                this.x = 1;
+                this.sx = -this.sx;
+            } else if(this.x > Juego.width) {
+                this.x = Juego.width -1;
+                this.sx = -this.sx;
+            }
+        },
+        collide: function(){
+            if(this.x >= Pala.x 
+                && this.x <= (Pala.x + Pala.w) 
+                && this.y >= Pala.y 
+                && this.y <= (Pala.y + Pala.h)){
+                    this.sx = 7 * ((this.x - (Pala.x + Pala.w / 2)) / Pala.w);
+                    this.sy = -this.sy;
+            }
+        },
+        
+        move: function(){
+            this.x += this.sx;
+            this.y += this.sy;
+        }
+    };
+
+    var Pantalla = {
+        bienvenida: function() {
+            this.text = 'REBOTE RICOCHET';
+            this.textSub = 'Clic para Iniciar';
+            this.textColor = 'white';
+            this.crear();
+        },
+        crear: function() {
+            ctx.fillStyle = 'black';
+            ctx.fillRect(0, 0, Juego.width, Juego.height);
+            ctx.fillStyle = this.textColor;
+            
+            ctx.textAlign = 'center';
+            ctx.font = '40px helvetica, arial';
+            ctx.fillText(this.text, Juego.width / 2, Juego.height / 2);
+            ctx.fillStyle = '#999999';
+            ctx.font = '20px helvetica, arial';
+            ctx.fillText(this.textSub, Juego.width / 2, Juego.height / 2 + 30);
+        },
+        juegoterminado: function() {
+            this.text = 'Juego Terminado';
+            this.textSub = 'Clic para Reintentar';
+            this.textColor = 'red';
+            this.crear();
+        }
+    };
+
+    var P3 = {
+        r: 10,
+        init: function() {
+            this.x = 140;
+            this.y = 140;
+            this.sx = 2;
+            this.sy = -2;
+        },
+
+        dibujar: function() {
+            this.edges();
+            this.collide();
+            this.move();
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+            ctx.closePath();
+            ctx.fillStyle = "#eee";
+            ctx.fill();
+        },
+
+        edges: function(){
+            if(this.y < 1){
+                this.y = 1;
+                this.sy = -this.sy;
+            } else if (this.y > Juego.height) {
+                this.sy = this.sx = 0;
+                this.y = this.x = 1000;
+                Pantalla.juegoterminado();
+                canvas.addEventListener('click', Juego.reiniciarJuego, false);
+                return;
+            }
+            if(this.x < 1){
+                this.x = 1;
+                this.sx = -this.sx;
+            } else if(this.x > Juego.width) {
+                this.x = Juego.width -1;
+                this.sx = -this.sx;
+            }
+        },
+        collide: function(){
+            if(this.x >= Pala.x 
+                && this.x <= (Pala.x + Pala.w) 
+                && this.y >= Pala.y 
+                && this.y <= (Pala.y + Pala.h)){
+                    this.sx = 7 * ((this.x - (Pala.x + Pala.w / 2)) / Pala.w);
+                    this.sy = -this.sy;
+            }
+        },
+        
+        move: function(){
+            this.x += this.sx;
+            this.y += this.sy;
+        }
+    };
     var Pala = {
         w: 90,
         h: 20,
@@ -331,7 +363,6 @@
 
         dibujar: function() {
             this.move();
-
             ctx.beginPath();
             ctx.moveTo(this.x, this.y);
             ctx.arcTo(this.x + this.w, this.y, this.x + this.w, this.y + this.r, this.r);
@@ -342,19 +373,16 @@
             ctx.lineTo(this.x, this.y + this.r);
             ctx.arcTo(this.x, this.y, this.x + this.r, this.y, this.r);
             ctx.closePath();
-
             ctx.fillStyle = this.gradient();
             ctx.fill();
         },
-
         move: function() {
-            if(Ctrl.left && (this.x < Juego.width - (this.w / 2))){ 
-                this.x += this.speed; 
-            } else if (Ctrl.right && this.x > - this.w / 2){
-                 this.x += -this.speed; 
-            }
+            if(Ctrl.left && (this.x < Juego.width - (this.w / 2))){
+                this.x += this.speed;
+                } else if (Ctrl.right && this.x > - this.w / 2){
+                this.x += -this.speed;
+                }
         },
-
         gradient: function() {
             if(this.gradientCache){
                 return this.gradientCache;
@@ -368,56 +396,56 @@
     };
 
     var Ctrl = {
-        init: function() { 
-            window.addEventListener('keydown', this.keyDown, true); 
-            window.addEventListener('keyup', this.keyUp, true); 
+        init: function() {
+            window.addEventListener('keydown', this.keyDown, true);
+            window.addEventListener('keyup', this.keyUp, true);
             window.addEventListener('mousemove', this.moverPala, true);
-
-            // Tactil
-            Juego.canvas.addEventListener('touchstart', this.moverPala, false); 
-            Juego.canvas.addEventListener('touchmove', this.moverPala, false); 
-            Juego.canvas.addEventListener('touchmove', this.stopTouchScroll, false);
-        }, 
-
-        stopTouchScroll: function(event) { 
-            event.preventDefault(); 
+        
         },
 
-        moverPala: function(event) { 
-            var mouseX = event.pageX; 
-            var canvasX = Juego.canvas.offsetLeft; 
-            var mitadPala = Pala.w / 2; 
-            if(mouseX > canvasX && mouseX < canvasX + Juego.width){ 
-                var nuevaX = mouseX - canvasX; nuevaX -= mitadPala;
-                Pala.x = nuevaX; 
+        keyDown: function(event){
+        switch(event.keyCode){
+            case 39: Ctrl.left = true; break;
+            case 37: Ctrl.right = true; break;
+            default:break;
             }
         },
-        
-        keyDown: function(event){ 
-            switch(event.keyCode){ 
-                case 39: Ctrl.left = true; 
-                break; 
-                case 37: Ctrl.right = true; 
-                break; 
-                default: 
-                break; 
-            } 
+        keyUp: function(event){
+            switch(event.keyCode){
+                case 39: Ctrl.left = false; break;
+                case 37: Ctrl.right = true; break;
+                default:break;
+            }
         },
 
-        keyUp: function(event){ 
-            switch(event.keyCode){ 
-                case 39: Ctrl.left = false; 
-                break; 
-                case 37: Ctrl.right = false; 
-                break; 
-                default: 
-                break; 
-            } 
-        }
+        moverPala: function(event) {
+            var mouseX = event.pageX;
+            var canvasX = Juego.canvas.offsetLeft;
+            var mitadPala = Pala.w / 2;
+            if(mouseX > canvasX && mouseX < canvasX + Juego.width){
+            var nuevaX = mouseX - canvasX;
+            nuevaX -= mitadPala;
+            Pala.x = nuevaX;
+            }
+            },
     };
+
+    var Tablero = {
+        init: function() {
+        this.nivel = 1;
+        this.marcador = 0;
+        },
+        dibujar: function() {
+        ctx.font='10px helvetica, arial';
+        ctx.fillStyle='white';
+        ctx.textAlign='left';
+        ctx.fillText('Marcador: '+this.marcador,5,Juego.height-5);
+        ctx.textAlign='right';
+        ctx.fillText('Niv:' + this.nivel, Juego.width - 5, Juego.height-5);
+        }
+        };
 
     window.onload = function() {
         Juego.configurar();
     };
 }());
-
